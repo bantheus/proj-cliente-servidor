18b9633e10f107aba0f485dc24ab0fe54b4195ab
Bantheus <matheuschmidt.b@gmail.com>
2023-05-11 21:59:02 -0300

    ajustes

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -19,8 +19,8 @@ export default function Signup() {
 
   return (
     <div className="bg-gray-800 min-h-screen flex flex-col">
-      <div className="container mx-auto flex-1 flex flex-row justify-center px-4 py-12 sm:px-6 lg:px-8">
-        <div className="max-w-md w-[50%] rounded-l-md max-h-[85vh] shadow-lg overflow-hidden">
+      <div className="container mx-auto flex-1 flex flex-row justify-center px-4 py-8 sm:px-6 lg:px-8">
+        <div className="max-w-md w-[50%] rounded-l-md max-h-[90vh] shadow-lg overflow-hidden hidden md:block">
           <div className="flex items-center justify-center h-full">
             <Image
               src="/road2.jpg"
@@ -33,13 +33,13 @@ export default function Signup() {
           </div>
         </div>
 
-        <div className="max-w-md w-[50%]  max-h-[85vh]">
-          <div className="bg-gray-700 shadow-lg rounded-e-md p-8 h-full">
-            <h3 className="text-2xl font-bold mb-4 text-center text-white">
+        <div className="max-w-md md:w-[50%] w-full max-h-[90vh]">
+          <div className="bg-gray-700 shadow-lg rounded-e-md md:rounded-s-none  rounded-s-md p-8 h-full">
+            <h3 className="text-3xl font-bold mb-4 text-center text-white">
               Sign up
             </h3>
 
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form className="space-y-4" onSubmit={handleSubmit}>
               <Input
                 tipo="text"
                 texto="Nome"
@@ -72,7 +72,7 @@ export default function Signup() {
             <div className="flex mt-6">
               <Link
                 href="/"
-                className="group mx-auto bg-gradient-to-r from-cyan-600 to-cyan-700 hover:from-cyan-700 hover:to-cyan-600 text-white font-bold py-2 px-4 rounded-md transition duration-300 ripple transform text-center w-[30%]"
+                className="group mx-auto bg-gradient-to-r from-cyan-600 to-cyan-700 hover:from-cyan-700 hover:to-cyan-600 text-white font-bold py-2 px-4 rounded-md transition duration-300 ripple transform text-center md:w-[50%]"
                 type="submit"
               >
                 <FontAwesomeIcon

97fc9b4bb7a97ccc13aa60024876dbf878b37c46
Bantheus <matheuschmidt.b@gmail.com>
2023-06-05 20:13:16 -0300

    ajustes navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,6 +2,7 @@ import { useAuthContext } from "@/hooks/useAuthContext";
 import { useLogout } from "@/hooks/useLogout";
 import Link from "next/link";
 import { useState } from "react";
+import { BiUserCircle } from "react-icons/bi";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,7 +22,7 @@ export default function Navbar() {
   return (
     <>
       <nav className="bg-gray-800">
-        <div className=" px-2 sm:px-6 lg:px-9">
+        <div className="px-2 sm:px-6 lg:px-9">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
@@ -31,15 +32,26 @@ export default function Navbar() {
               </div>
             </div>
 
-            {user && (
-              <p className="hidden text-sm font-medium text-gray-300 md:block">
-                <a href="/usuario">
-                  Bem-vindo <span className="text-cyan-500">{user.email}</span>
-                </a>
-              </p>
-            )}
+            <div className="hidden md:block">
+              {user && (
+                <p className="text-sm font-medium text-gray-300">
+                  <a href="/usuario">
+                    Bem-vindo{" "}
+                    <span className="text-cyan-500">{user.email}</span>
+                  </a>
+                </p>
+              )}
+            </div>
 
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:hidden">
+              {user && (
+                <div className="mr-4">
+                  <a href="/usuario">
+                    <BiUserCircle className="h-6 w-6 text-cyan-500" />
+                  </a>
+                </div>
+              )}
+
               <button
                 type="button"
                 className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
